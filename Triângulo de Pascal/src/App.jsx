b5c3ae58317a2430c4eef129583809aa10a24f3b
Anna <annafelferrer@gmail.com>
2025-08-29 11:57:32 -0300

    triangulo de pascal

---
+++ Triângulo de Pascal/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <input placeholder='insira até qual linha você deseja imprimir o triângulo de pascal' type="number" />
+
+  )
+}
+
+export default App
